ffac38e2cb4097151fca47cbbeeba2e82033104a
mjh5153 <mjh5153@gmail.com>
2023-01-08 12:48:42 -0500

    Generate and Add Assembler code versions 2 and 3-final

---
+++ hhw3.s
@@ -0,0 +1,38 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 11, 0	sdk_version 11, 3
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	%edi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
+	movq	-16(%rbp), %rax
+	cmpq	$0, (%rax)
+	jne	LBB0_3
+## %bb.2:
+	jmp	LBB0_4
+LBB0_3:                                 ##   in Loop: Header=BB0_1 Depth=1
+	movq	-16(%rbp), %rax
+	movq	%rax, %rcx
+	addq	$8, %rcx
+	movq	%rcx, -16(%rbp)
+	movq	(%rax), %rdi
+	callq	_puts
+	jmp	LBB0_1
+LBB0_4:
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+.subsections_via_symbols
